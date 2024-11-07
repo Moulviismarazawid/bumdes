aa9df420b25c4e126beecc38d0facbe32089c901
Moulviismar <mongkipag31@gmail.com>
2024-11-07 23:36:35 +0700

    handle login error

--- src/components/views/auth/Login/index.tsx
+++ src/components/views/auth/Login/index.tsx
@@ -18,26 +18,40 @@ const LoginView = () =>{
         setIsLoading(true)
         setError('')
         const form = e.target as HTMLFormElement
-         try{
-            const res = await signIn('credentials', {
-                redirect: false,
-                email: form.email.value,
-                password: form.password.value,
-                callbackUrl
-            })
-            if(!res?.error){
-                setIsLoading(false)
-                form.reset();
-                push(callbackUrl)
-            }else{
-                setIsLoading(false)
-                setError("Invalid email or password")
-            }
-        }catch(error){
-            setIsLoading(false)
-            console.log(error)
-            setError("Invalid email or password")
+        const data = {
+            email: form.email.value,
+            password: form.password.value
         }
+        const result = await signIn('credentials', {
+            redirect: false,
+            ...data
+        })
+        if(result && !result.error){
+            push(callbackUrl)
+        }else{
+            setIsLoading(false)
+            setError('Invalid credentials')
+        }
+        //          try{
+        //     const res = await signIn('credentials', {
+        //         redirect: false,
+        //         email: form.email.value,
+        //         password: form.password.value,
+        //         callbackUrl
+        //     })
+        //     if(!res?.error){
+        //         setIsLoading(false)
+        //         form.reset();
+        //         push(callbackUrl)
+        //     }else{
+        //         setIsLoading(false)
+        //         setError("Invalid email or password")
+        //     }
+        // }catch(error){
+        //     setIsLoading(false)
+        //     console.log(error)
+        //     setError("Invalid email or password")
+        // }
     }
     
 
